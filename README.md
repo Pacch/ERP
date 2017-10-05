311e310c671aaa0b08aa0000f306efbd70619dd9
Harol Esquivel <harolesquivel92@gmail.com>
2017-10-05 10:53:24 -0500

    readme modificacion

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # ERP
+
+=====================
+
+Proyecto
+-------------------
+[Enlace al demo](https://pacch.github.io/ERP/)
+
+Resumen del proyecto
+--------------------
+
+El proyecto ha sido realizado con una plantilla, la cual se fue modificando segun se creia conveniente.
+
+* HTML5
+* CSS3
+
+Librerias externas:
+No se usaron librerias externas.
+
+La web se hizo como un trabajo freelance para la empresa Art Web System , no soy el autor del diseño. 
+[Enlace del template.](http://trendyblog.different-themes.com/)
+
+El proyecto terminado y Lanzado
+
+[Enlace al demo](http://peru-erp.com/)
+
+
+
+
+
---
+++ form_ingreso.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Registro</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="keywords" content="que es sap, que es erp, trabajo it, consultoría erp, sistemas erp, sistema de gestión empresarial, mrp, sistemas rrhh, recursos humanos, sistema contable, sistema ventas, sistema facturación, sistema inventarios, sistemas logisticos, crm, sistemas, erp, consultoría, contable, software, implementación, contabilidad, ventas, facturacion, logistica, tesorería, inventarios, procure to pay, cuentas por cobrar, cuentas por pagar, activos fijos, contabilidad general, cuentas mayor, general ledger, consultoría sap, consultoría oracle, consultoría openerp, erp, xendra, compire, openbravo, lima, crm, peru, gestión, empresarial, sap, dynamic, ebs, código abierto, consultoria de sistemas, perú" />
+    <meta name="description" content="PERU-ERP.COM, consultoría e implementación de sistemas de gestión empresarial erp en el Perú." />
+    <meta name="author" content="ERP PERU">
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="img/favicon.ico"/>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <link rel="stylesheet" href="css/weather.css">
+    <link rel="stylesheet" href="css/colors.css">
+    <link rel="stylesheet" href="css/typography.css">
+    <link rel="stylesheet" href="css/style.css">
+     
+    <!-- Responsive -->
+    <link rel="stylesheet" type="text/css" media="(max-width:768px)" href="css/responsive-0.css">
+    <link rel="stylesheet" type="text/css" media="(min-width:769px) and (max-width:992px)" href="css/responsive-768.css">
+    <link rel="stylesheet" type="text/css" media="(min-width:993px) and (max-width:1200px)" href="css/responsive-992.css">
+    <link rel="stylesheet" type="text/css" media="(min-width:1201px)" href="css/responsive-1200.css">
+    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:300,300italic,400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <!-- Wrapper -->
+    <div id="wrapper" class="wide">            
+        <!-- Header -->
+        <header id="header">              
+            <!-- Header meta -->
+            <div class="header_meta">
+                <div class="container">
+                    <!-- Weather forecast -->
+                           <div class="weather_forecast responsive-header">
+
+                        <div class="derecha">
+                            <span class="correo"><i class="fa fa-envelope" aria-hidden="true"></i>informes@peru-erp.com</span>
+                            <span class="telefono"><i class="fa fa-phone-square" aria-hidden="true"></i>956 496 167</span>    
+                        </div>
+                        
+                    </div><!-- End Weather forecast -->
+                </div>
+            </div><!-- End Header meta -->
+            <!-- Header main -->
+            <div id="header_main" class="sticky header_main">
+                <div class="container">
+                    <!-- Logo -->
+                    <div class="site_brand">
+                        <div class="particion-logo">
+                        <h2 id="site_title"><a href="index.html"><img src="img/logotipo_peru-erp.png" alt="ERP-PERU"></a></h2> 
+                        </div>
+                    </div><!-- End Logo -->
+                    <!-- Site navigation -->
+                   <nav class="site_navigation">
+                        <span class="site_navigation_toggle"><i class="fa fa-reorder"></i></span>
+                        <ul class="menu">
+                            <li class="menu-item-has-children"><a href="index.html">Inicio</a>
+                            </li>
+                            <li class="menu-item-has-children"><a href="nosotros.html">Nosotros</a>
+                            </li>
+                            <li class="menu-item-has-children"><a href="servicios.html">Servicios</a></li>
+                            <li class="menu-item-has-children"><a href="capacitacion.html">Capacitacion</a></li>
+                            <li class="menu-item-has-children"><a href="novedades.html">Novedades</a>
+                            </li>
+                            <li class="menu-item-has-children"><a href="empleos.html">Empleos</a>
+                            </li>
+
+                            <li><a href="contacto.html">Contácto</a></li>
+                        </ul>
+                    </nav><!-- End Site navigation -->
+                    <!-- Site navigation -->
+                </div>
+            </div><!-- End Header main -->
+        </header><!-- End Header -->
+        <!-- Section -->
+        <section>
+            <div class="container">
+                <div class="row">
+                    <!-- Main content -->
+                    <div class="col col_9_of_12">
+                        <!-- Panel title -->
+                        <div class="panel_title">
+                                <div>
+                                    <h4>Formulario de Ingreso</h4>
+                                </div>
+                        </div>
+                        <div class="row">
+                            <div class="col col_12_of_12">
+                                <div class=" clearfix">
+                                    <!-- Post lists 1 -->
+                                    <div class="list_posts">
+                                        <!-- Post -->
+                                        <div class="post clearfix">
+                                            <div class="item_content espacio">
+                                            <h4><a href="#"><i class="fa fa-chevron-right" aria-hidden="true"></i> Ingreso</a></h4>
+                                            <br>
+                                            <h4><a href="#"> Datos Personales</a></h4>
+                                            <div class="content-registro">
+                                                <form class="formulario-registro">
+                                                    <div >
+                                                        <div class="conteiner-registro">
+                                                            <label for="registro"> Correo Electrónico: (*)</label>
+                                                            <input type="text" name="Nombres" value="" placeholder="Ingrese su nombre aqui">
+                                                        </div>
+                                                        <br>
+                                                        <div class="conteiner-registro">
+                                                            <label for="registro"> Clave: (*)</label>
+                                                            <input type="text" name="clave" value="" placeholder="Ingrese su clave aqui" >
+                                                        </div>
+                                                        <br>
+                                                        <div class="botones">
+                                                            <a href="#" class="btn btn_red">Ingresar</a>
+                                                            <a href="#" class="btn btn_red">Limpiar</a>    
+                                                        </div>
+                                                    </div>                    
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div><!-- End Post -->                                     
+                                    </div><!-- End Post lists -->
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- End Main content -->
+                    <!-- Sidebar -->
+                    <div class="col col_3_of_12">
+                        <!-- Widget Social widget -->
+                        <div class="widget">
+                            <div class="widget_title"><h3>Social</h3></div>
+                            <div class="tb_widget_socialize clearfix">
+                                <a href="http://www.facebook.com/" target="_blank" class="icon facebook">
+                                    <div class="symbol"><i class="fa fa-facebook"></i></div>
+                                </a>
+                                <a href="https://plus.google.com" target="_blank"  class="icon google">
+                                    <div class="symbol"><i class="fa fa-google-plus"></i></div>
+                                </a>
+                                <a href="http://www.twitter.com/" target="_blank"  class="icon twitter">
+                                    <div class="symbol"><i class="fa fa-twitter"></i></div>
+                                </a>
+                                <a href="http://www.linkedin.com/" target="_blank"  class="icon linkedin">
+                                    <div class="symbol"><i class="fa fa-linkedin"></i></div>
+                                </a>
+                            </div>
+                            <br>
+                        <div class="widget">
+                            <div class="widget_title"><h3>Publicidad</h3></div>
+                            <div class="tb_widget_banner_125 clearfix">
+                                <a href="contacto.html" target="_blank">
+                                    <img src="demo/125x125.png" alt="Banner">
+                                </a>
+                                <a href="contacto.html" target="_blank">
+                                    <img src="demo/125x125.png" alt="Banner">
+                                </a>
+                                <a href="contacto.html" target="_blank">
+                                    <img src="demo/125x125.png" alt="Banner">
+                                </a>
+                                <a href="contacto.html" target="_blank">
+                                    <img src="demo/125x125.png" alt="Banner">
+                                </a>
+                            </div>
+                        </div><!-- End Widget Banners 125 -->
+                    </div><!-- End Sidebar -->
+                   </div>
+                </div>
+                <!-- Panel title -->
+            </div>
+        </section><!-- End Section -->
+        <!-- Footer -->
+        <footer id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col col_12_of_12 footer-div">
+                        <ul class="lista-footer">
+                            <li><a href="legal.html">Legal</a></li>
+                            <li><a href="contacto.html">Contácto</a></li>
+                            <li><a href="politicas.html">Políticas</a> </li>
+                            <li><a href="publicidad.html">Publicidad</a>   </li>
+                            <li><a href="enlaces.html">Enlaces</a></li>
+                            <li><a href="mapa_sitio.html">Mapa Del Sitio</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer><!-- End Footer --><!-- Copyright -->
+        <div id="copyright" role="contentinfo">
+            <div class="container">
+                <p> © Copyright 2008-2017 PERU ERP | Derechos Reservados - Lima - Perú </p>
+            </div>
+        </div><!-- End Copyright -->
+    </div><!-- End Wrapper -->
+    
+    <!-- Scripts -->
+    <script type="text/javascript" src="js/jqueryscript.min.js"></script>
+    <script type="text/javascript" src="js/jqueryuiscript.min.js"></script>
+    <script type="text/javascript" src="js/easing.min.js"></script>        
+    <script type="text/javascript" src="js/smoothscroll.min.js"></script>        
+    <script type="text/javascript" src="js/magnific.min.js"></script>        
+    <script type="text/javascript" src="js/bxslider.min.js"></script>        
+    <script type="text/javascript" src="js/fitvids.min.js"></script>
+    <script type="text/javascript" src="js/viewportchecker.min.js"></script>        
+    <script type="text/javascript" src="js/init.js"></script>        
+</body>
+</html>
